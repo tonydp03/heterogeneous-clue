c66f1f3728c8523483b7571cda8abab4589c338c
Tony Di Pilato <41752419+tonydp03@users.noreply.github.com>
2022-10-18 09:41:35 +0200

    [cuda] [cudatest] Added cuda-CLUE (#7)
    * [alpaka] Added plugin PointsLoader and Validator (cannot be tested as it needs the Clusterizer first
    
    * Implemented Kernels and CLUEClusterizer - BROKEN
    
    * Updated file read and points load
    
    * Fixed CLUE Algo execution and added OutputProducer
    
    * Added CLUE validator and moved from alpakatest to alpaka
    
    * Modified Source file and updated alpakatest
    
    * Delete configurationCache.log
    
    * Delete dryrun.log
    
    * Delete c_cpp_properties.json
    
    * Delete targets.log
    
    * Delete settings.json
    
    * first commit for cuda implementation
    
    * [cuda] CUDADataFormats and plugin-CLUEClusterizer, compiles
    
    * remove unnecessary files
    
    * Setup framework for serial implementation
    
    * not working, problem with global wait
    
    * [cuda] cuda version working
    
    * Ported kernels to serial functions
    
    * Added OutputProducer
    
    * Added Validator plugin
    
    * fix
    
    * output producer
    
    * cleaning
    
    * small fix and validator
    
    * Completed CUDA implementation and updated cudatest
    
    * Updated cuda to match alpaka (CLUE 2D only)
    
    Co-authored-by: Wahid Redjeb <wahid.redjeb.wr@gmail.com>

---
+++ src/cuda/plugin-CLUEClusterizer/CLUEAlgoCUDA.cu
@@ -0,0 +1,75 @@
+#include <math.h>
+#include <limits>
+#include <iostream>
+
+// GPU Add
+#include <cuda_runtime.h>
+#include <cuda.h>
+// for timing
+#include <chrono>
+#include <ctime>
+// user include
+
+#include "CLUEAlgoCUDA.h"
+#include "CLUEAlgoKernels.h"
+#include "CUDACore/cudaCheck.h"
+
+void CLUEAlgoCUDA::init_device() {
+  d_hist = cms::cuda::make_device_unique<LayerTilesCUDA[]>(NLAYERS, stream_);
+  d_seeds = cms::cuda::make_device_unique<cms::cuda::VecArray<int, maxNSeeds>>(stream_);
+  d_followers = cms::cuda::make_device_unique<cms::cuda::VecArray<int, maxNFollowers>[]>(reserve, stream_);
+
+  hist_ = d_hist.get();
+  seeds_ = d_seeds.get();
+  followers_ = d_followers.get();
+}
+
+void CLUEAlgoCUDA::setup(PointsCloud const& host_pc) {
+  // copy input variables
+  cudaCheck(cudaMemcpyAsync(
+      d_points.x.get(), host_pc.x.data(), sizeof(float) * host_pc.x.size(), cudaMemcpyHostToDevice, stream_));
+  cudaCheck(cudaMemcpyAsync(
+      d_points.y.get(), host_pc.y.data(), sizeof(float) * host_pc.x.size(), cudaMemcpyHostToDevice, stream_));
+  cudaCheck(cudaMemcpyAsync(
+      d_points.layer.get(), host_pc.layer.data(), sizeof(int) * host_pc.x.size(), cudaMemcpyHostToDevice, stream_));
+  cudaCheck(cudaMemcpyAsync(
+      d_points.weight.get(), host_pc.weight.data(), sizeof(float) * host_pc.x.size(), cudaMemcpyHostToDevice, stream_));
+  // initialize result and internal variables
+  // // result variables
+  cudaCheck(cudaMemsetAsync(d_points.rho.get(), 0x00, sizeof(float) * host_pc.x.size(), stream_));
+  cudaCheck(cudaMemsetAsync(d_points.delta.get(), 0x00, sizeof(float) * host_pc.x.size(), stream_));
+  cudaCheck(cudaMemsetAsync(d_points.nearestHigher.get(), 0x00, sizeof(int) * host_pc.x.size(), stream_));
+  cudaCheck(cudaMemsetAsync(d_points.clusterIndex.get(), 0x00, sizeof(int) * host_pc.x.size(), stream_));
+  cudaCheck(cudaMemsetAsync(d_points.isSeed.get(), 0x00, sizeof(int) * host_pc.x.size(), stream_));
+  // algorithm internal variables
+  cudaCheck(cudaMemsetAsync(d_hist.get(), 0x00, sizeof(LayerTilesCUDA) * NLAYERS, stream_));
+  cudaCheck(cudaMemsetAsync(d_seeds.get(), 0x00, sizeof(cms::cuda::VecArray<int, maxNSeeds>), stream_));
+  cudaCheck(cudaMemsetAsync(
+      d_followers.get(), 0x00, sizeof(cms::cuda::VecArray<int, maxNFollowers>) * host_pc.x.size(), stream_));
+
+  cudaStreamSynchronize(stream_);
+}
+
+void CLUEAlgoCUDA::makeClusters(PointsCloud const& host_pc) {
+  setup(host_pc);
+  ////////////////////////////////////////////
+  // calculate rho, delta and find seeds
+  // 1 point per thread
+  ////////////////////////////////////////////
+  const dim3 blockSize(1024, 1, 1);
+  const dim3 gridSize(ceil(host_pc.x.size() / static_cast<float>(blockSize.x)), 1, 1);
+  kernel_compute_histogram<<<gridSize, blockSize, 0, stream_>>>(d_hist.get(), d_points.view(), host_pc.x.size());
+  kernel_calculate_density<<<gridSize, blockSize, 0, stream_>>>(d_hist.get(), d_points.view(), dc_, host_pc.x.size());
+  kernel_calculate_distanceToHigher<<<gridSize, blockSize, 0, stream_>>>(
+      d_hist.get(), d_points.view(), outlierDeltaFactor_, dc_, host_pc.x.size());
+  kernel_find_clusters<<<gridSize, blockSize, 0, stream_>>>(
+      d_seeds.get(), d_followers.get(), d_points.view(), outlierDeltaFactor_, dc_, rhoc_, host_pc.x.size());
+
+  ////////////////////////////////////////////
+  // assign clusters
+  // 1 point per seeds
+  ////////////////////////////////////////////
+  const dim3 gridSize_nseeds(ceil(maxNSeeds / static_cast<float>(blockSize.x)), 1, 1);
+  kernel_assign_clusters<<<gridSize_nseeds, blockSize, 0, stream_>>>(d_seeds.get(), d_followers.get(), d_points.view());
+  cudaStreamSynchronize(stream_);
+}
